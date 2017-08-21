d5cafc31e4a39cfe4682043e9accb16419d41fa7
belardgx <30979712+belardgx@users.noreply.github.com>
2017-08-21 23:02:45 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAGapDg4dEABACZA8uEc5mUbBylq0SkSidrKX3P2SBQEtIoeVWcI8izcxqCRA0KmCzidwikWvvkZAH21fKZBMHu6pb83YBESRYs2y5IGe0bqPzZCfmvUKawZCSXyZC3CUuqv8AdxNrVER85vZAzsr8NXlZAZCz8R0cFZBahZCUClyC3BQZDZD',
+    FB_VERIFY_TOKEN: 'gabieta_verify_token',
+    API_AI_CLIENT_ACCESS_TOKEN: '7b2569d5848c48ddbc13d6e0b9112ac5',
+    FB_APP_SECRET: '97baf5f6cce327fcaeb2c7a8c6cae41e',
+    SERVER_URL: "https://intense-crag-77008.herokuapp.com/",
+    SENGRID_API_KEY: "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",
+    EMAIL_FROM: "glenn.belardo@ymail.com",
+    EMAIL_TO: "glenn.belado@ymail.com"
+};
